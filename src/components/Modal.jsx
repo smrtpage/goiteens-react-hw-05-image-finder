8e984655a6d48863ced1ea02a13b6a32e7b4a601
Igor-Lysak <lisakigor485@gmail.com>
2023-06-22 21:00:25 +0300

    update target

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
+
 function Modal({ largeImageURL, onClose }) {
+  useEffect(() => {
+    window.addEventListener("keydown", keydown);
+    return () => {
+      window.removeEventListener("keydown", keydown);
+    };
+  }, []);
+  function overlayClick(e) {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  }
+
+  function keydown(e) {
+    if (e.code === "Escape") {
+      onClose();
+    }
+  }
   return (
     <div>
-      <div onClick={onClose} onKeyDown={onClose} className="Overlay">
+      <div onClick={overlayClick} className="Overlay">
         <div className="Modal">
-          <img onKeyDown={onClose} src={largeImageURL} />
+          <img src={largeImageURL} />
         </div>
       </div>
     </div>
